27c97fdd08e6608ce8c2f76477e4a657bfe52347
Olli <86614494+Olli1989@users.noreply.github.com>
2022-03-24 13:57:29 +0100

    Final Project

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -9,6 +11,34 @@ public class Main {
 				"Russland","Kanada","Schweden","Südkorea","Schweiz"};
 		int[][] medals = {{11, 12, 6}, {9, 9, 7}, {9, 7, 7}, {8, 6, 8},
 				{7, 10, 7}, {7, 2, 5}, {6, 3, 2}, {5, 4, 5}};
-		System.out.println("test");
+
+		int sum;
+
+		for (int i = 0; i < countries.length; i++) {
+			sum=0;
+			System.out.print((i+1) + "." + countries[i] + ": ");
+			for (int j = 0; j < medals[i].length; j++) {
+				if(j!=medals[i].length-1){
+					System.out.print(medals[i][j] + ",");
+				} else {
+					System.out.print(medals[i][j]);
+				}
+				sum+=medals[i][j];
+			}
+			System.out.print(" -> Medals in total: "+sum);
+			System.out.println();
+		}
+
+		int[] newArr = getAllSilverMedals(medals);
+		System.out.println(Arrays.toString(newArr));
 	}
+
+	public static int[] getAllSilverMedals(int[][] medals){
+		int[] silverMedals = new int[medals.length];
+		for (int i = 0; i < medals.length; i++) {
+			silverMedals[i] = medals[i][1];
+		}
+		return silverMedals;
+	}
+
 }
